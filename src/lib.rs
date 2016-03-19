65736345a9fe7eea4e608d0f42b8a93aa8d5a419
Curtis McEnroe <programble@gmail.com>
2016-03-19 17:05:19 -0400

    Add basic BoundedInteger trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,34 @@
-#[cfg(test)]
-mod test {
-    #[test]
-    fn it_works() {
-    }
+//! Bounded integers.
+
+#![warn(
+    missing_docs,
+    missing_debug_implementations,
+    missing_copy_implementations,
+    trivial_casts,
+    trivial_numeric_casts,
+    unused_extern_crates,
+    unused_import_braces,
+    unused_qualifications,
+    unused_results,
+    variant_size_differences,
+)]
+
+/// Marker trait for integer representations.
+pub trait Repr: Copy { }
+
+impl Repr for u8 { }
+impl Repr for u16 { }
+impl Repr for u32 { }
+impl Repr for u64 { }
+
+/// Bounded integer.
+pub trait BoundedInteger: Copy {
+    /// Integer representation.
+    type Repr: Repr;
+
+    /// Converts from representation to Self.
+    fn from_repr(repr: Self::Repr) -> Option<Self>;
+
+    /// Converts from Self to representation.
+    fn to_repr(self) -> Self::Repr;
 }
