09b9ac8f315b1cabd81895f5f127f55c50eeb769
Curtis McEnroe <programble@gmail.com>
2016-03-20 23:55:40 -0400

    Add bounded_integer_div_{self,repr}_impls! macros

--- src/lib.rs
+++ src/lib.rs
@@ -409,3 +409,79 @@ macro_rules! bounded_integer_mul_repr_impls {
         }
     }
 }
+
+/// Implements `std::ops::Div` for a `BoundedInteger` enum with `Self`.
+///
+/// Implements the following combinations. The `Output` is always `Self`.
+///
+/// - `Self / Self`
+/// - `Self / &Self`
+/// - `&Self / Self`
+/// - `&Self / &Self`
+///
+/// The implementations always panic on overflow.
+#[macro_export]
+macro_rules! bounded_integer_div_self_impls {
+    ($ty:ty) => {
+        impl ::std::ops::Div<$ty> for $ty {
+            type Output = Self;
+            fn div(self, rhs: Self) -> Self {
+                use $crate::BoundedInteger;
+                self.checked_div(rhs).expect("arithmetic operation overflowed")
+            }
+        }
+
+        impl<'a> ::std::ops::Div<&'a $ty> for $ty {
+            type Output = Self;
+            fn div(self, rhs: &Self) -> Self { self / *rhs }
+        }
+
+        impl<'a> ::std::ops::Div<$ty> for &'a $ty {
+            type Output = $ty;
+            fn div(self, rhs: $ty) -> $ty { *self / rhs }
+        }
+
+        impl<'a, 'b> ::std::ops::Div<&'b $ty> for &'a $ty {
+            type Output = $ty;
+            fn div(self, rhs: &$ty) -> $ty { *self / *rhs }
+        }
+    }
+}
+
+/// Implements `std::ops::Div` for a `BoundedInteger` enum with `Self::Repr`.
+///
+/// Implements the following combinations. The `Output` is always `Self`.
+///
+/// - `Self / Self::Repr`
+/// - `Self / &Self::Repr`
+/// - `&Self / Self::Repr`
+/// - `&Self / &Self::Repr`
+///
+/// The implementations always panic on overflow.
+#[macro_export]
+macro_rules! bounded_integer_div_repr_impls {
+    ($ty:ty) => {
+        impl ::std::ops::Div<<$ty as $crate::BoundedInteger>::Repr> for $ty {
+            type Output = Self;
+            fn div(self, rhs: <$ty as $crate::BoundedInteger>::Repr) -> Self {
+                use $crate::BoundedInteger;
+                self.checked_div_repr(rhs).expect("arithmetic operation overflowed")
+            }
+        }
+
+        impl<'a> ::std::ops::Div<&'a <$ty as $crate::BoundedInteger>::Repr> for $ty {
+            type Output = Self;
+            fn div(self, rhs: &'a <$ty as $crate::BoundedInteger>::Repr) -> Self { self / *rhs }
+        }
+
+        impl<'a> ::std::ops::Div<<$ty as $crate::BoundedInteger>::Repr> for &'a $ty {
+            type Output = $ty;
+            fn div(self, rhs: <$ty as $crate::BoundedInteger>::Repr) -> $ty { *self / rhs }
+        }
+
+        impl<'a, 'b> ::std::ops::Div<&'b <$ty as $crate::BoundedInteger>::Repr> for &'a $ty {
+            type Output = $ty;
+            fn div(self, rhs: &<$ty as $crate::BoundedInteger>::Repr) -> $ty { *self / *rhs }
+        }
+    }
+}
