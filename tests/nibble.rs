69f92d88ad2601070359892141ff8ddda73921b3
Curtis McEnroe <programble@gmail.com>
2016-03-20 22:34:49 -0400

    Test BoundedInteger::checked_mul

---
+++ tests/nibble.rs
@@ -0,0 +1,46 @@
+#[macro_use]
+extern crate bounded_integer;
+
+use bounded_integer::BoundedInteger;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Ord)]
+#[allow(dead_code)]
+#[repr(u8)]
+enum Nibble {
+    N8 = 248, N7, N6, N5, N4, N3, N2, N1, U0 = 0, P1, P2, P3, P4, P5, P6, P7
+}
+
+bounded_integer_impl!(Nibble, i8, Nibble::N8, Nibble::P7);
+bounded_integer_partial_ord_impl!(Nibble);
+
+#[test]
+fn checked_mul() {
+    assert_eq!(Some(Nibble::P4), Nibble::P2.checked_mul(Nibble::P2));
+    assert_eq!(Some(Nibble::N4), Nibble::P2.checked_mul(Nibble::N2));
+    assert_eq!(None, Nibble::P4.checked_mul(Nibble::P2));
+    assert_eq!(None, Nibble::N8.checked_mul(Nibble::P2));
+}
+
+#[test]
+fn checked_mul_repr() {
+    assert_eq!(Some(Nibble::P4), Nibble::P2.checked_mul_repr(2));
+    assert_eq!(Some(Nibble::N4), Nibble::P2.checked_mul_repr(-2));
+    assert_eq!(None, Nibble::P4.checked_mul_repr(2));
+    assert_eq!(None, Nibble::N8.checked_mul_repr(2));
+}
+
+#[test]
+fn saturating_mul() {
+    assert_eq!(Nibble::P7, Nibble::P4.saturating_mul(Nibble::P2));
+    assert_eq!(Nibble::P7, Nibble::N4.saturating_mul(Nibble::N2));
+    assert_eq!(Nibble::N8, Nibble::N8.saturating_mul(Nibble::P2));
+    assert_eq!(Nibble::N8, Nibble::P2.saturating_mul(Nibble::N8));
+}
+
+#[test]
+fn saturating_mul_repr() {
+    assert_eq!(Nibble::P7, Nibble::P4.saturating_mul_repr(2));
+    assert_eq!(Nibble::P7, Nibble::N4.saturating_mul_repr(-2));
+    assert_eq!(Nibble::N8, Nibble::N8.saturating_mul_repr(2));
+    assert_eq!(Nibble::N8, Nibble::P2.saturating_mul_repr(-8));
+}
