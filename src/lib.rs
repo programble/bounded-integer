9618ee57dddae88667d8faff61e80e6fe3d53aba
Curtis McEnroe <programble@gmail.com>
2016-03-22 23:42:39 -0400

    Rearrange items of BoundedInteger trait

--- src/lib.rs
+++ src/lib.rs
@@ -24,26 +24,23 @@ pub trait BoundedInteger: Copy + Eq + Ord {
     /// Integer representation.
     type Repr: Repr;
 
-    /// Converts from representation to Self.
+    /// Converts from `Self::Repr` to `Self`.
     fn from_repr(repr: Self::Repr) -> Option<Self>;
 
-    /// Converts from Self to representation.
+    /// Converts from `Self` to `Self::Repr`.
     fn to_repr(self) -> Self::Repr;
 
-    /// Returns the smallest value that can be represented as Self.
+    /// Returns the smallest value that can be represented as `Self`.
     fn min_value() -> Self;
 
-    /// Returns the largest value that can be represented as Self.
+    /// Returns the largest value that can be represented as `Self`.
     fn max_value() -> Self;
+
+    // Checked arithmetic.
 
     /// Checked integer addition.
     fn checked_add(self, other: Self) -> Option<Self> {
         self.checked_add_repr(other.to_repr())
-    }
-
-    /// Checked integer addition with representation.
-    fn checked_add_repr(self, other: Self::Repr) -> Option<Self> {
-        self.to_repr().checked_add(other).and_then(Self::from_repr)
     }
 
     /// Checked integer subtraction.
@@ -51,19 +48,9 @@ pub trait BoundedInteger: Copy + Eq + Ord {
         self.checked_sub_repr(other.to_repr())
     }
 
-    /// Checked integer subtraction with representation.
-    fn checked_sub_repr(self, other: Self::Repr) -> Option<Self> {
-        self.to_repr().checked_sub(other).and_then(Self::from_repr)
-    }
-
     /// Checked integer multiplication.
     fn checked_mul(self, other: Self) -> Option<Self> {
         self.checked_mul_repr(other.to_repr())
-    }
-
-    /// Checked integer multiplication with representation.
-    fn checked_mul_repr(self, other: Self::Repr) -> Option<Self> {
-        self.to_repr().checked_mul(other).and_then(Self::from_repr)
     }
 
     /// Checked integer division.
@@ -71,19 +58,9 @@ pub trait BoundedInteger: Copy + Eq + Ord {
         self.checked_div_repr(other.to_repr())
     }
 
-    /// Checked integer division with representation.
-    fn checked_div_repr(self, other: Self::Repr) -> Option<Self> {
-        self.to_repr().checked_div(other).and_then(Self::from_repr)
-    }
-
     /// Checked integer remainder.
     fn checked_rem(self, other: Self) -> Option<Self> {
         self.checked_rem_repr(other.to_repr())
-    }
-
-    /// Checked integer remainder with representation.
-    fn checked_rem_repr(self, other: Self::Repr) -> Option<Self> {
-        self.to_repr().checked_rem(other).and_then(Self::from_repr)
     }
 
     /// Checked integer negation.
@@ -91,12 +68,53 @@ pub trait BoundedInteger: Copy + Eq + Ord {
         self.to_repr().checked_neg().and_then(Self::from_repr)
     }
 
+    // Checked arithmetic with `Self::Repr`.
+
+    /// Checked integer addition with `Self::Repr`.
+    fn checked_add_repr(self, other: Self::Repr) -> Option<Self> {
+        self.to_repr().checked_add(other).and_then(Self::from_repr)
+    }
+
+    /// Checked integer subtraction with `Self::Repr`.
+    fn checked_sub_repr(self, other: Self::Repr) -> Option<Self> {
+        self.to_repr().checked_sub(other).and_then(Self::from_repr)
+    }
+
+    /// Checked integer multiplication with `Self::Repr`.
+    fn checked_mul_repr(self, other: Self::Repr) -> Option<Self> {
+        self.to_repr().checked_mul(other).and_then(Self::from_repr)
+    }
+
+    /// Checked integer division with `Self::Repr`.
+    fn checked_div_repr(self, other: Self::Repr) -> Option<Self> {
+        self.to_repr().checked_div(other).and_then(Self::from_repr)
+    }
+
+    /// Checked integer remainder with `Self::Repr`.
+    fn checked_rem_repr(self, other: Self::Repr) -> Option<Self> {
+        self.to_repr().checked_rem(other).and_then(Self::from_repr)
+    }
+
+    // Saturating arithmetic.
+
     /// Saturating integer addition.
     fn saturating_add(self, other: Self) -> Self {
         self.saturating_add_repr(other.to_repr())
     }
 
-    /// Saturating integer addition with representation.
+    /// Saturating integer subtraction.
+    fn saturating_sub(self, other: Self) -> Self {
+        self.saturating_sub_repr(other.to_repr())
+    }
+
+    /// Saturating integer multiplication.
+    fn saturating_mul(self, other: Self) -> Self {
+        self.saturating_mul_repr(other.to_repr())
+    }
+
+    // Saturating arithmetic with `Self::Repr`.
+
+    /// Saturating integer addition with `Self::Repr`.
     fn saturating_add_repr(self, other: Self::Repr) -> Self {
         if other.is_negative() {
             self.checked_add_repr(other).unwrap_or(Self::min_value())
@@ -105,12 +123,7 @@ pub trait BoundedInteger: Copy + Eq + Ord {
         }
     }
 
-    /// Saturating integer subtraction.
-    fn saturating_sub(self, other: Self) -> Self {
-        self.saturating_sub_repr(other.to_repr())
-    }
-
-    /// Saturating integer subtraction with representation.
+    /// Saturating integer subtraction with `Self::Repr`.
     fn saturating_sub_repr(self, other: Self::Repr) -> Self {
         if other.is_negative() {
             self.checked_sub_repr(other).unwrap_or(Self::max_value())
@@ -119,12 +132,7 @@ pub trait BoundedInteger: Copy + Eq + Ord {
         }
     }
 
-    /// Saturating integer multiplication.
-    fn saturating_mul(self, other: Self) -> Self {
-        self.saturating_mul_repr(other.to_repr())
-    }
-
-    /// Saturating integer multiplication with representation.
+    /// Saturating integer multiplication with `Self::Repr`.
     fn saturating_mul_repr(self, other: Self::Repr) -> Self {
         if self.to_repr().is_negative() == other.is_negative() {
             self.checked_mul_repr(other).unwrap_or(Self::max_value())
