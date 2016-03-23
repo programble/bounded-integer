fe1842b4157e8a487c71b56d61020ce098e785a5
Curtis McEnroe <programble@gmail.com>
2016-03-23 00:23:10 -0400

    Rename Nibble to SNibble

--- tests/nibble.rs
+++ tests/nibble.rs
@@ -6,60 +6,60 @@ use bounded_integer::BoundedInteger;
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Ord)]
 #[allow(dead_code)]
 #[repr(u8)]
-enum Nibble {
+enum SNibble {
     N8 = 248, N7, N6, N5, N4, N3, N2, N1, U0 = 0, P1, P2, P3, P4, P5, P6, P7
 }
-bounded_integer_impls!(Nibble, i8, Nibble::N8, Nibble::P7);
-bounded_integer_partial_ord_impl!(Nibble);
+bounded_integer_impls!(SNibble, i8, SNibble::N8, SNibble::P7);
+bounded_integer_partial_ord_impl!(SNibble);
 
 #[test]
 fn from_repr() {
-    assert_eq!(Some(Nibble::U0), Nibble::from_repr(0i8));
-    assert_eq!(Some(Nibble::N8), Nibble::from_repr(-8i8));
-    assert_eq!(Some(Nibble::P7), Nibble::from_repr(7i8));
+    assert_eq!(Some(SNibble::U0), SNibble::from_repr(0i8));
+    assert_eq!(Some(SNibble::N8), SNibble::from_repr(-8i8));
+    assert_eq!(Some(SNibble::P7), SNibble::from_repr(7i8));
 }
 
 #[test]
 fn to_repr() {
-    assert_eq!(0i8, Nibble::U0.to_repr());
-    assert_eq!(-8i8, Nibble::N8.to_repr());
-    assert_eq!(7i8, Nibble::P7.to_repr());
+    assert_eq!(0i8, SNibble::U0.to_repr());
+    assert_eq!(-8i8, SNibble::N8.to_repr());
+    assert_eq!(7i8, SNibble::P7.to_repr());
 }
 
 #[test]
 fn min_value() {
-    assert_eq!(Nibble::N8, Nibble::min_value());
+    assert_eq!(SNibble::N8, SNibble::min_value());
 }
 
 #[test]
 fn max_value() {
-    assert_eq!(Nibble::P7, Nibble::max_value());
+    assert_eq!(SNibble::P7, SNibble::max_value());
 }
 
 #[test]
 fn checked_add() {
-    assert_eq!(Some(Nibble::P3), Nibble::P1.checked_add(Nibble::P2));
-    assert_eq!(Some(Nibble::N3), Nibble::N1.checked_add(Nibble::N2));
-    assert_eq!(Some(Nibble::N1), Nibble::P1.checked_add(Nibble::N2));
-    assert_eq!(None, Nibble::P7.checked_add(Nibble::P1));
-    assert_eq!(None, Nibble::N8.checked_add(Nibble::N1));
+    assert_eq!(Some(SNibble::P3), SNibble::P1.checked_add(SNibble::P2));
+    assert_eq!(Some(SNibble::N3), SNibble::N1.checked_add(SNibble::N2));
+    assert_eq!(Some(SNibble::N1), SNibble::P1.checked_add(SNibble::N2));
+    assert_eq!(None, SNibble::P7.checked_add(SNibble::P1));
+    assert_eq!(None, SNibble::N8.checked_add(SNibble::N1));
 }
 
 #[test]
 fn checked_sub() {
-    assert_eq!(Some(Nibble::P1), Nibble::P3.checked_sub(Nibble::P2));
-    assert_eq!(Some(Nibble::N1), Nibble::N3.checked_sub(Nibble::N2));
-    assert_eq!(Some(Nibble::P1), Nibble::N1.checked_sub(Nibble::N2));
-    assert_eq!(None, Nibble::N8.checked_sub(Nibble::P1));
-    assert_eq!(None, Nibble::P7.checked_sub(Nibble::N1));
+    assert_eq!(Some(SNibble::P1), SNibble::P3.checked_sub(SNibble::P2));
+    assert_eq!(Some(SNibble::N1), SNibble::N3.checked_sub(SNibble::N2));
+    assert_eq!(Some(SNibble::P1), SNibble::N1.checked_sub(SNibble::N2));
+    assert_eq!(None, SNibble::N8.checked_sub(SNibble::P1));
+    assert_eq!(None, SNibble::P7.checked_sub(SNibble::N1));
 }
 
 #[test]
 fn checked_mul() {
-    assert_eq!(Some(Nibble::P6), Nibble::P2.checked_mul(Nibble::P3));
-    assert_eq!(Some(Nibble::P6), Nibble::N2.checked_mul(Nibble::N3));
-    assert_eq!(Some(Nibble::N6), Nibble::N2.checked_mul(Nibble::P3));
-    assert_eq!(None, Nibble::P2.checked_mul(Nibble::P4));
-    assert_eq!(None, Nibble::N2.checked_mul(Nibble::N4));
-    assert_eq!(None, Nibble::N3.checked_mul(Nibble::P3));
+    assert_eq!(Some(SNibble::P6), SNibble::P2.checked_mul(SNibble::P3));
+    assert_eq!(Some(SNibble::P6), SNibble::N2.checked_mul(SNibble::N3));
+    assert_eq!(Some(SNibble::N6), SNibble::N2.checked_mul(SNibble::P3));
+    assert_eq!(None, SNibble::P2.checked_mul(SNibble::P4));
+    assert_eq!(None, SNibble::N2.checked_mul(SNibble::N4));
+    assert_eq!(None, SNibble::N3.checked_mul(SNibble::P3));
 }
