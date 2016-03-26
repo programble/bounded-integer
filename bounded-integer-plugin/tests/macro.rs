745d9ec9a723b3107bc86e67c7d97f9fc8fb5bc1
Curtis McEnroe <programble@gmail.com>
2016-03-26 13:57:44 -0400

    Add bounded_integer invocations to test

--- bounded-integer-plugin/tests/macro.rs
+++ bounded-integer-plugin/tests/macro.rs
@@ -4,4 +4,18 @@
 #[macro_use]
 extern crate bounded_integer;
 
-bounded_integer!();
+bounded_integer! {
+    pub enum Bit { 0...1 }
+}
+
+bounded_integer! {
+    enum UNibble { 0..15 }
+}
+
+bounded_integer! {
+    enum SNibble { -8..7 }
+}
+
+bounded_integer! {
+    enum NZUNibble { 1..15 }
+}
