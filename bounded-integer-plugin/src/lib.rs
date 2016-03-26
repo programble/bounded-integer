dbf187e725aaa7b9651f624ee68212954f4a25e9
Curtis McEnroe <programble@gmail.com>
2016-03-26 16:23:39 -0400

    Parse ident

--- bounded-integer-plugin/src/lib.rs
+++ bounded-integer-plugin/src/lib.rs
@@ -16,12 +16,18 @@ extern crate syntax;
 extern crate rustc_plugin;
 
 use rustc_plugin::Registry;
-use syntax::ast::TokenTree;
+use syntax::ast::{TokenTree, Ident};
 use syntax::codemap::Span;
 use syntax::ext::base::{ExtCtxt, MacResult, DummyResult};
 use syntax::errors::DiagnosticBuilder;
 use syntax::parse;
 use syntax::parse::token::keywords::Keyword;
+
+#[derive(Debug)]
+struct IntEnum {
+    is_pub: bool,
+    ident: Ident,
+}
 
 #[plugin_registrar]
 pub fn plugin_registrar(reg: &mut Registry) {
@@ -33,18 +39,28 @@ fn expand_bounded_integer(
     sp: Span,
     tts: &[TokenTree]
 ) -> Box<MacResult + 'static> {
-    match parse_tts(cx, tts) {
-        Err(mut e) => e.emit(),
-        _ => (),
+    let int_enum = match parse_tts(cx, tts) {
+        Ok(ie) => ie,
+        Err(mut err) => {
+            err.emit();
+            return DummyResult::any(sp);
+        }
     };
+
+    println!("{:?}", int_enum);
+
     DummyResult::any(sp)
 }
 
-fn parse_tts<'a>(cx: &'a mut ExtCtxt, tts: &[TokenTree]) -> Result<(), DiagnosticBuilder<'a>> {
+fn parse_tts<'a>(cx: &'a mut ExtCtxt, tts: &[TokenTree]) -> Result<IntEnum, DiagnosticBuilder<'a>> {
     let mut parser = parse::new_parser_from_tts(cx.parse_sess(), cx.cfg(), tts.to_vec());
 
     let is_pub = parser.eat_keyword(Keyword::Pub);
     try!(parser.expect_keyword(Keyword::Enum));
+    let ident = try!(parser.parse_ident());
 
-    Ok(())
+    Ok(IntEnum {
+        is_pub: is_pub,
+        ident: ident,
+    })
 }
