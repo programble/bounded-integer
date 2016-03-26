43b2bb238fa2d32e068242cf46414b740608870a
Curtis McEnroe <programble@gmail.com>
2016-03-26 16:06:11 -0400

    Begin macro parser

--- bounded-integer-plugin/src/lib.rs
+++ bounded-integer-plugin/src/lib.rs
@@ -19,6 +19,9 @@ use rustc_plugin::Registry;
 use syntax::ast::TokenTree;
 use syntax::codemap::Span;
 use syntax::ext::base::{ExtCtxt, MacResult, DummyResult};
+use syntax::errors::DiagnosticBuilder;
+use syntax::parse;
+use syntax::parse::token::keywords::Keyword;
 
 #[plugin_registrar]
 pub fn plugin_registrar(reg: &mut Registry) {
@@ -26,9 +29,22 @@ pub fn plugin_registrar(reg: &mut Registry) {
 }
 
 fn expand_bounded_integer(
-    _context: &mut ExtCtxt,
-    span: Span,
-    _args: &[TokenTree]
+    cx: &mut ExtCtxt,
+    sp: Span,
+    tts: &[TokenTree]
 ) -> Box<MacResult + 'static> {
-    DummyResult::any(span)
+    match parse_tts(cx, tts) {
+        Err(mut e) => e.emit(),
+        _ => (),
+    };
+    DummyResult::any(sp)
+}
+
+fn parse_tts<'a>(cx: &'a mut ExtCtxt, tts: &[TokenTree]) -> Result<(), DiagnosticBuilder<'a>> {
+    let mut parser = parse::new_parser_from_tts(cx.parse_sess(), cx.cfg(), tts.to_vec());
+
+    let is_pub = parser.eat_keyword(Keyword::Pub);
+    try!(parser.expect_keyword(Keyword::Enum));
+
+    Ok(())
 }
