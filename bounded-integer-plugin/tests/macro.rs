2e29dc3eea1ba5011952ce173279ce6eb15634db
Curtis McEnroe <programble@gmail.com>
2016-03-27 12:09:27 -0400

    Test added derives

--- bounded-integer-plugin/tests/macro.rs
+++ bounded-integer-plugin/tests/macro.rs
@@ -2,10 +2,13 @@
 #![feature(plugin)]
 #![plugin(bounded_integer_plugin)]
 
-use std::hash::Hash;
+use std::fmt::Debug;
 
 trait AssertDefined { }
-trait AssertImplHash: Hash { }
+trait AssertImplDebug: Debug { }
+trait AssertImplCopy: Copy { }
+trait AssertImplEq: Eq { }
+trait AssertImplOrd: Ord { }
 
 bounded_integer! { enum A: u8 { 0...5 } }
 impl AssertDefined for A { }
@@ -16,8 +19,14 @@ mod b {
 impl AssertDefined for b::B { }
 
 bounded_integer! {
-    #[derive(Hash)]
+    #[derive(Debug)]
     enum C: u8 { 0...5 }
 }
 impl AssertDefined for C { }
-impl AssertImplHash for C { }
+impl AssertImplDebug for C { }
+
+bounded_integer! { enum D: u8 { 0...5 } }
+impl AssertDefined for D { }
+impl AssertImplCopy for D { }
+impl AssertImplEq for D { }
+impl AssertImplOrd for D { }
